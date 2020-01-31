f32551b8da60da1e59892827d7deb1575586e133
Robert Grądzki <robertx.gradzki@intel.com>
2020-01-31 02:05:50 +0100

    django ver

--- django/notejam/notejam/settings.py
+++ django/notejam/notejam/settings.py
@@ -14,7 +14,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'notejam.db',
         'USER': '',
         'PASSWORD': '',
