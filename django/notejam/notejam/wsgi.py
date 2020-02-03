5f1aa832e82c5250c59db1a713a7c088cca7c20f
Robert Grądzki <robertx.gradzki@intel.com>
2020-02-03 01:03:50 +0100

    fix

--- django/notejam/notejam/wsgi.py
+++ django/notejam/notejam/wsgi.py
@@ -16,7 +16,7 @@ framework.
 import os
 import sys
 sys.path = [os.path.abspath(os.path.dirname(__file__))] + sys.path
-print ("RGSYSPATH: " + sys.path)
+print (sys.path)
 # import sys
 
 # myPath = os.path.dirname(os.path.realpath(__file__))
