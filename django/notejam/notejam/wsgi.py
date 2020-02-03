4101efab3fbd7ae80e89127a9c3a9659098c3f9d
Robert Grądzki <robertx.gradzki@intel.com>
2020-02-03 01:21:21 +0100

    fix

--- django/notejam/notejam/wsgi.py
+++ django/notejam/notejam/wsgi.py
@@ -23,7 +23,7 @@ print (sys.path)
 # if myPath not in sys.path:
 #     sys.path.append(myPath)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notejam.notejam.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "notejam.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
